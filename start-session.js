bd9e07a181bf65c646760b4e6d50352b868f1214
Joey Parrish <joey.parrish@gmail.com>
2025-03-11 13:20:22 -0700

    Fix key visualizer app

--- start-session.js
+++ start-session.js
@@ -4,8 +4,15 @@ window.addEventListener('DOMContentLoaded', () => {
   if (window.cast && cast.__platform__) {
     console.log('Starting Cast receiver session.');
     const context = cast.framework.CastReceiverContext.getInstance();
+
     context.start({
+      // Keep the app on screen.
       disableIdleTimeout: true,
+
+      // Receive keyboard events for HDMI-CEC.
+      uiConfig: {
+        touchScreenOptimizedApp: true,
+      },
     });
   }
 });
